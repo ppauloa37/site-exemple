6f449ee70c0b0deb794a888285ea4a86835aa439
ppauloa37 <ppauloa37@gmail.com>
2025-02-10 20:40:53 -0300

    Update execut.py

--- execut.py
+++ execut.py
@@ -1,1 +1,9 @@
+from cx_Freeze import setup, Executable
+
+setup(
+    name="PaintApp",
+    version="0.1 beta",
+    description="Aplicativo de Desenho",
+    executables=[Executable("paint.py", base="Win32GUI")]
+)
 
