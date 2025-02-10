8b86eb80a3e03657af333a2f07f19c83f86d485d
ppauloa37 <ppauloa37@gmail.com>
2025-02-10 20:33:40 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+    var contDown = document.getElementById("contdown1");
+    
+    var onContDown = function() {
+        contDown.textContent = parseFloat(contDown.textContent) - 1
+        
+        
+        
+    };
